b8b6d8c623f438fe593fc4459b9377b5a63170dc
kimhyun <forhn37@gmail.com>
2023-10-05 18:15:20 +0900

    서버 구성

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+const http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end("안녕")
+}).listen(5050)
